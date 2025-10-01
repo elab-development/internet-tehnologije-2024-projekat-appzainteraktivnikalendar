bf2942703a919efcb43616ed24d0816df8b4a171
peca998 <57746421+peca998@users.noreply.github.com>
2025-10-01 20:02:37 +0200

    - add SK3 (pregled sopstvenih termina)
    - fix role target

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\PatientController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,3 +12,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
 });
 
+Route::middleware(['auth:sanctum', 'role:patient'])->group(function () {
+    Route::get('my-calendar', [PatientController::class, 'myCalendar']);
+});
